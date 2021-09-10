6368433a903783f3c3decb8f6b42dca2bdf1430e
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-09-11 03:25:54 +0700

    drop counter folder

--- src/features/admin/AdminDashboard.js
+++ src/features/admin/AdminDashboard.js
@@ -1,13 +1,11 @@
 import React from "react";
 // import AddminAddBook from "./AdminAddBook";
-import AdminRentBook from "./AdminRentBook";
 
 const AdminDashboard = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
       {/* <AddminAddBook /> */}
-      <AdminRentBook />
     </div>
   );
 };
