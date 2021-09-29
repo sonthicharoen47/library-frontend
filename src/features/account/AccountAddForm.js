b92f0690029c8c2dd78070597e24e34307f2373e
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-09-29 14:06:17 +0700

    update css on register page

--- src/features/account/AccountAddForm.js
+++ src/features/account/AccountAddForm.js
@@ -22,7 +22,19 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DatePicker from "@mui/lab/DatePicker";
 
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import SvgRegister from "../../picture/undraw_Waiting__for_you_ldha.svg";
+
+import VpnKeyOutlinedIcon from "@mui/icons-material/VpnKeyOutlined";
+import { styled } from "@mui/material/styles";
+import { amber } from "@mui/material/colors";
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(amber[600]),
+  backgroundColor: amber[600],
+  "&:hover": {
+    backgroundColor: amber[700],
+  },
+}));
 
 const regex = /^[0-9\b]+$/;
 const dateMax = new Date();
@@ -88,127 +100,167 @@ const AccountAddForm = () => {
 
   return (
     <React.Fragment>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 6,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            sx={{ mt: 2 }}
-            onSubmit={handleSubmit}
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Grid container direction="row">
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                marginTop: 12,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                bgcolor: "background.paper",
+                overflow: "hidden",
+                borderRadius: "16px",
+                boxShadow: 3,
+                fontWeight: "bold",
+                mx: 16,
+                px: 3,
+              }}
+            >
+              <Avatar
+                sx={{
+                  m: 2,
+                  width: 50,
+                  height: 50,
+                  bgcolor: "#3949ab",
+                }}
+              >
+                <VpnKeyOutlinedIcon sx={{ fontSize: 40 }} />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign up
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                sx={{ mt: 2 }}
+                onSubmit={handleSubmit}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="fname"
+                      name="firstName"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                      value={fname}
+                      onChange={onFameChanged}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="lname"
+                      value={lname}
+                      onChange={onLnameChanged}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={onEmailChanged}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      value={password}
+                      onChange={onPasswordChanged}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                      <DatePicker
+                        disableFuture
+                        label="Date of birth"
+                        maxDate={dateMax}
+                        value={dob}
+                        onChange={(newValue) => {
+                          setDob(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                      />
+                    </LocalizationProvider>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="phone"
+                      label="Phone Number"
+                      name="phone"
+                      autoComplete="phone"
+                      value={phone}
+                      type="tel"
+                      onChange={onPhoneChanged}
+                      inputProps={{ maxLength: 10, pattern: "[0-9]*" }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}></Grid>
+                </Grid>
+                <ColorButton
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  Sign Up
+                </ColorButton>
+                <Grid container justifyContent="flex-end">
+                  <Grid item sx={{ m: 2 }}>
+                    <Link href="/login" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sx={{ justifyContent: "center", alignItems: "center" }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  value={fname}
-                  onChange={onFameChanged}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                  value={lname}
-                  onChange={onLnameChanged}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={onEmailChanged}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  value={password}
-                  onChange={onPasswordChanged}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    disableFuture
-                    label="Date of birth"
-                    maxDate={dateMax}
-                    value={dob}
-                    onChange={(newValue) => {
-                      setDob(newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="phone"
-                  label="Phone Number"
-                  name="phone"
-                  autoComplete="phone"
-                  value={phone}
-                  type="tel"
-                  onChange={onPhoneChanged}
-                  inputProps={{ maxLength: 10, pattern: "[0-9]*" }}
-                />
-              </Grid>
-              <Grid item xs={12}></Grid>
-            </Grid>
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Box mt={5}></Box>
+            <Box
+              sx={{
+                width: "80vh",
+                height: "90vh",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              component="img"
+              src={SvgRegister}
+              alt="login please"
+            />
+          </Grid>
+        </Grid>
       </Container>
     </React.Fragment>
   );
