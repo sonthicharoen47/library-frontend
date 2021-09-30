ef2e450e0ad2267e7934181d6d44b36364582ac0
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-09-30 20:59:52 +0700

    update css navbar

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { signoutAccount, logout } from "../account/accountSlice";
 import { postSnackbarAlert } from "../snackbarAlert/snackbarAlertsSlice";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import MenuBookOutlinedIcon from "@mui/icons-material/MenuBookOutlined";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 
 //css
 import {
@@ -12,13 +15,37 @@ import {
   Toolbar,
   Typography,
   Button,
+  Slide,
+  Box,
 } from "@mui/material";
+
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+
+import { styled } from "@mui/material/styles";
+import { deepPurple, purple } from "@mui/material/colors";
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(deepPurple[600]),
+  backgroundColor: deepPurple[600],
+  "&:hover": {
+    backgroundColor: deepPurple[400],
+  },
+}));
+
+const HomeButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[900]),
+  backgroundColor: purple[900],
+  "&:hover": {
+    backgroundColor: deepPurple[700],
+  },
+}));
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { user, isLogged, token } = useSelector((state) => state.accounts);
   const [logInView, setLogInView] = useState("hidden");
+  const trigger = useScrollTrigger();
 
   const handleLogout = async () => {
     await dispatch(logout());
@@ -50,83 +77,72 @@ const Navbar = () => {
     return (
       <React.Fragment>
         <CssBaseline />
-        {user && user.role === "user" ? (
+
+        <Slide appear={false} direction="down" in={!trigger}>
           <AppBar
-            position="static"
+            style={{ padding: 0, margin: 0 }}
+            position="sticky"
             color="default"
             elevation={0}
             sx={{
-              bordetrBottom: (theme) => `1px solid ${theme.palette.divider}`,
+              borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
               visibility: logInView,
+              minHeight: 56,
+              bgcolor: "#4a148c",
             }}
           >
-            <Toolbar sx={{ flexWrap: "wrap" }}>
-              <Typography
-                variant="h3"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
+            <Toolbar
+              style={{ minHeight: 56 }}
+              sx={{ flexWrap: "wrap" }}
+              disableGutters={true}
+            >
+              <HomeButton
+                sx={{ minHeight: 56, px: 2, color: "#ede7f6" }}
+                startIcon={<MenuBookOutlinedIcon />}
+                onClick={() => history.push("/dashboard")}
               >
-                <Button onClick={() => history.push("/dashboard")}>
-                  Library Online
-                </Button>
-              </Typography>
-              <nav>
-                <Link
-                  to="/dashboard/selected"
-                  style={{ textDecoration: "none" }}
-                >
-                  <Button type="button">BookSelected</Button>
-                </Link>
-                <Link
-                  to="/orderhistory"
-                  style={{ textDecoration: "none", height: "100%" }}
-                >
-                  <Button type="button">history</Button>
-                </Link>
-              </nav>
-
+                Library Online
+              </HomeButton>
+              <Box sx={{ flexGrow: 1 }} />
+              {user && user.role === "user" ? (
+                <Box sx={{ display: "flex" }}>
+                  <Link
+                    to="/dashboard/selected"
+                    style={{ textDecoration: "none", marginRight: "8px" }}
+                  >
+                    <ColorButton
+                      variant="contained"
+                      startIcon={<AddShoppingCartOutlinedIcon />}
+                      sx={{ minHeight: 40, bgcolor: "#673ab7" }}
+                    >
+                      BookSelected
+                    </ColorButton>
+                  </Link>
+                  <Link
+                    to="/orderhistory"
+                    style={{ textDecoration: "none", marginRight: "8px" }}
+                  >
+                    <ColorButton
+                      variant="contained"
+                      startIcon={<HistoryOutlinedIcon />}
+                      sx={{ minHeight: 40 }}
+                    >
+                      history
+                    </ColorButton>
+                  </Link>
+                </Box>
+              ) : null}
               <Button
-                variant="outlined"
-                sx={{ my: 1, mx: 1.5 }}
+                sx={{ minHeight: 40, mr: 1 }}
+                variant="contained"
                 onClick={handleLogout}
+                color="error"
               >
                 Logout
               </Button>
             </Toolbar>
           </AppBar>
-        ) : (
-          <AppBar
-            position="static"
-            color="default"
-            elevation={0}
-            sx={{
-              bordetrBottom: (theme) => `1px solid ${theme.palette.divider}`,
-              visibility: logInView,
-            }}
-          >
-            <Toolbar sx={{ flexWrap: "wrap" }}>
-              <Typography
-                variant="h3"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-              >
-                <Button onClick={() => history.push("/admin/dashboard")}>
-                  Library Online
-                </Button>
-              </Typography>
-
-              <Button
-                variant="outlined"
-                sx={{ my: 1, mx: 1.5 }}
-                onClick={handleLogout}
-              >
-                Logout
-              </Button>
-            </Toolbar>
-          </AppBar>
-        )}
+        </Slide>
       </React.Fragment>
     );
   } else {
